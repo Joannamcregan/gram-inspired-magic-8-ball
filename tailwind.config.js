d5620eca445cfaede71a3853bf45110507c4a780
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2024-03-31 12:26:02 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/*.{html,js}',
   './index.html'],
   theme: {
+    screens: {
+      'xs580': '580px',
+      'xs530': '530px',
+      'xs480': '480px',
+      'xs430': '430px',
+      'xs380': '380px',
+      'xs330': '330px',
+      'xs280': '280px',
+      ...defaultTheme.screens,
+    },
     extend: {
         'width':{
           '128': '32rem',
+          '7/10': '70%',
+          'xs500': '500px',
+          'xs450': '450px',
+          'xs400': '400px',
+          'xs350': '350px',
+          'xs300': '300px',
+          'xs250': '250px',
         },
         'height':{
           '128': '32rem',
+          '7/10': '70%',
+          'xs500': '500px',
+          'xs450': '450px',
+          'xs400': '400px',
+          'xs350': '350px',
+          'xs300': '300px',
+          'xs250': '250px',
         },
         'backgroundImage':{
           'black-radial-gradient': 'radial-gradient(farthest-corner at 10px 40px, #333, #000)',
